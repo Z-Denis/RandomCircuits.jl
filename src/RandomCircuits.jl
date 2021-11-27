b8f3ea2b81283022aa96f805e9201356a1fc3816
Z-Denis <zakari.denis@univ-paris-diderot.fr>
2021-11-27 23:13:41 +0100

    Update includes and imports

--- src/RandomCircuits.jl
+++ src/RandomCircuits.jl
@@ -1,8 +1,13 @@
 module RandomCircuits
 
 using QuantumOpticsBase, QuantumInformation
+using LinearAlgebra, SparseArrays, ElasticArrays
+using OrdinaryDiffEq, DiffEqBase, DiffEqCallbacks
 
 include("types.jl")
 export AbstractGate, HaarGate, hamiltonian, RowGate, generate_random_circuit
+
+include("timeevolution.jl")
+export compute_tstops, time_evolution
 
 end
